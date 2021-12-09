789ab977be3410d93637c15190fe2abad8fb793a
donchodonev <dummyacc9292@gmail.com>
2021-12-09 16:02:18 +0200

    previous exercises solution is 100/100, so it this one, but this makes more sense

--- 2.ARRAYS AND NESTED ARRAYS/2.Exercise/6.List of Names.js
+++ 2.ARRAYS AND NESTED ARRAYS/2.Exercise/6.List of Names.js
@@ -1,9 +1,10 @@
 function printNamesSorted(nameArray) {
-    let result = [];
-
-    nameArray
-        .sort((a,b) => a.localeCompare(b))
-        .reduce((acc, c, index) => result.push(`${index + 1}.${c}`), []);
+    let result = nameArray
+        .sort((a, b) => a.localeCompare(b))
+        .reduce((acc, c, index) => {
+            acc.push(`${index + 1}.${c}`)
+            return acc
+        }, []);
 
     console.log(result.join('\n'));
 }
