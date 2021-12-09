9cd2b671192dc88086c4130aa0bd8ecb97e52987
donchodonev <dummyacc9292@gmail.com>
2021-12-09 13:24:42 +0200

    added another exercise solution

---
+++ 2.ARRAYS AND NESTED ARRAYS/4.Negative Or Positive Numbers.js
@@ -0,0 +1,13 @@
+function sortNumbersAscending(array) {
+    let result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            result.unshift(array[i]);
+        } else {
+            result.push(array[i]);
+        }
+    }
+
+    console.log(result.join('\n'));
+}
