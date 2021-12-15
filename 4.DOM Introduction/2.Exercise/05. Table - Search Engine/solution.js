c2d19fd13d98b68e28c17215f3f4da6431b4c45d
donchodonev <dummyacc9292@gmail.com>
2021-12-15 16:41:28 +0200

    added another exercise solution

--- 4.DOM Introduction/2.Exercise/05. Table - Search Engine/solution.js
+++ 4.DOM Introduction/2.Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,22 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+   document
+      .querySelector('#searchBtn')
+      .addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let tableRows = document.getElementsByTagName('tr');
+      let searchTerm = document.getElementById('searchField').value;
 
+      for (const row of tableRows) {
+         for (const child of row.children) {
+            if (searchTerm && searchTerm !== " " && child.innerText.includes(searchTerm) && !child.innerText.includes('Student')) {
+               row.setAttribute('class', 'select');
+               break;
+            }
+            row.removeAttribute('class');
+         }
+      }
+
+      document.getElementById('searchField').value = '';
    }
 }
