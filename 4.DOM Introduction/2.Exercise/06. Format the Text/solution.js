dc61b64a76ea68e839544183166572a9e3b17db0
donchodonev <dummyacc9292@gmail.com>
2021-12-15 17:35:28 +0200

    added another exercise solution

--- 4.DOM Introduction/2.Exercise/06. Format the Text/solution.js
+++ 4.DOM Introduction/2.Exercise/06. Format the Text/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO
+  const input = document.querySelector('#input').value;
+  const sentences = input.split('.').filter(x => x);
+  const output = document.querySelector('#output');
+
+  let p = document.createElement('p');
+  let counter = 0;
+
+  for (let i = 0; i < sentences.length; i++, counter++) {
+    p.textContent += sentences[i] + '.';
+
+    if (counter == 2 || sentences.length - 1 == i) {
+      output.appendChild(p);
+      counter = -1;
+      p = document.createElement('p');
+    }
+  }
 }
