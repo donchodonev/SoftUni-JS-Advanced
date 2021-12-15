ca277a9f3ae1fa5831788701758d9c929423ae7e
donchodonev <dummyacc9292@gmail.com>
2021-12-15 13:49:36 +0200

    added first exercise solution

---
+++ 4.DOM Introduction/2.Exercise/01. Subtraction/subtract.js
@@ -0,0 +1,13 @@
+function subtract() {
+    let num1 = Number(document
+        .getElementById('firstNumber')
+        .value);
+
+    let num2 = Number(document
+        .getElementById('secondNumber')
+        .value);
+
+    let res = num1 - num2;
+
+    document.getElementById('result').textContent = res;
+}
