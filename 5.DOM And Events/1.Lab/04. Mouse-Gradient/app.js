c29f3e8836c1576155a8b6ec92a7f4fb14ff375e
donchodonev <dummyacc9292@gmail.com>
2021-12-16 15:30:47 +0200

    added another exercise solution

--- 5.DOM And Events/1.Lab/04. Mouse-Gradient/app.js
+++ 5.DOM And Events/1.Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,18 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+
+    const gradient = document.getElementById('gradient');
+
+    gradient.addEventListener('mousemove', onHover);
+
+    function onHover(ev) {
+        const target = ev.target;
+        const elWidth = target.clientWidth;
+        const xOffset = ev.offsetX;
+
+        const result = document.getElementById('result');
+
+        let percent = Math.floor((xOffset / elWidth) * 100);
+
+        result.textContent = `${percent}%`;
+    }
 }
