4d29ec40c422b6772c2914dc4ccd263ca1f77816
donchodonev <dummyacc9292@gmail.com>
2021-12-16 14:54:02 +0200

    added another exercise solution

--- 5.DOM And Events/1.Lab/03. Add-Delete/app.js
+++ 5.DOM And Events/1.Lab/03. Add-Delete/app.js
@@ -1,3 +1,34 @@
 function addItem() {
-    //TODO...
+    const itemTextContent = document.getElementById('newItemText').value;
+
+    const item = elFactory('li', itemTextContent);
+
+    let parent = document.getElementById('items');
+
+    parent.addEventListener('click', removeListItem)
+
+    parent.appendChild(item);
+
+    function removeListItem(ev) {
+        const target = ev.target;
+
+        if (target.nodeName == 'A' && target.textContent == '[Delete]') {
+            target.parentNode.remove();
+        }
+    }
+
+    function elFactory(elType, textContent) {
+        let el = document.createElement(elType);
+
+        el.textContent = textContent;
+
+        let anchor = document.createElement('a');
+
+        anchor.href = '#';
+        anchor.textContent = '[Delete]'
+
+        el.appendChild(anchor);
+
+        return el;
+    }
 }
