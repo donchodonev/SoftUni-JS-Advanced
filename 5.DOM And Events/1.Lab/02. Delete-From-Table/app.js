b4348eca23966998c3ffdbf31e71f5849a655b71
donchodonev <dummyacc9292@gmail.com>
2021-12-16 14:28:44 +0200

    added another exercise solution

--- 5.DOM And Events/1.Lab/02. Delete-From-Table/app.js
+++ 5.DOM And Events/1.Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,18 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const rows = Array.from(document.querySelectorAll('tbody tr td'));
+
+    const email = document.querySelector('input[name="email"]').value;
+
+    const rowsContainingEmail = rows.filter(x => x.textContent == email);
+
+    let emailFound = false;
+
+    for (const row of rowsContainingEmail) {
+        row.parentNode.remove();
+        emailFound = true;
+    }
+
+    document
+        .getElementById('result')
+        .textContent = emailFound ? "Deleted." : "Not found.";
 }
