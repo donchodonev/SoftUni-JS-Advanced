089d8dd646136711acbff2f6bb4f2f30d0728fdb
donchodonev <dummyacc9292@gmail.com>
2021-12-16 14:07:38 +0200

    added first exercise solution

--- 5.DOM And Events/1.Lab/01. List-Of-Items/app.js
+++ 5.DOM And Events/1.Lab/01. List-Of-Items/app.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    const li = document.createElement('li');
+    
+    const ul = document.getElementById('items');
+
+    const newLiValue = document.getElementById('newItemText').value;
+
+    li.textContent = newLiValue;
+
+    ul.appendChild(li);
 }
