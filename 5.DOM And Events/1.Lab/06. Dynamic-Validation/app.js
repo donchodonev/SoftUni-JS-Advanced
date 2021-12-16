1c88143a873a46f110e4968aa1493107fd4ed1ac
donchodonev <dummyacc9292@gmail.com>
2021-12-16 16:21:36 +0200

    added another exercise solution

--- 5.DOM And Events/1.Lab/06. Dynamic-Validation/app.js
+++ 5.DOM And Events/1.Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,20 @@
 function validate() {
-    console.log('TODO:...');
+    const input = document.getElementById('email');
+
+    input.addEventListener('change', onChange);
+
+    function onChange(ev) {
+        const target = ev.target;
+
+        const email = target.value;
+
+        const regex = new RegExp(/^[a-z]+@[a-z]+\.[a-z]+$/gm);
+
+        if (regex.test(email)) {
+            target.classList.remove('error')
+        }
+        else {
+            target.classList.add('error')
+        }
+    }
 }
