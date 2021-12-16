e623d62ea59ed8b96ed2c5a09c058b8dcfbfaad7
donchodonev <dummyacc9292@gmail.com>
2021-12-16 16:07:09 +0200

    added another exercise solution

--- 5.DOM And Events/1.Lab/05. Highlight-Active/app.js
+++ 5.DOM And Events/1.Lab/05. Highlight-Active/app.js
@@ -1,3 +1,18 @@
 function focused() {
-    console.log('TODO:...');
+    const divs = Array.from(document.querySelectorAll('div input'));
+
+    divs.forEach(function (div) {
+        div.addEventListener('focus', onFocus);
+        div.addEventListener('blur', onBlur);
+    });
+
+    function onFocus(ev) {
+        const parent = ev.target.parentNode;
+        parent.classList.add('focused');
+    }
+
+    function onBlur(ev) {
+        const parent = ev.target.parentNode;
+        parent.classList.remove('focused');
+    }
 }
