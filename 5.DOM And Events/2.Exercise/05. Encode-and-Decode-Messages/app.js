8595eb163b9389c3ddb81a617ba5e27f51954b1a
donchodonev <dummyacc9292@gmail.com>
2021-12-17 12:35:39 +0200

    added another exercise solution

--- 5.DOM And Events/2.Exercise/05. Encode-and-Decode-Messages/app.js
+++ 5.DOM And Events/2.Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,35 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const [encodeSend, decodeRead] = document.getElementsByTagName('button');
+    const [inputArea, outputArea] = document.getElementsByTagName('textarea');
+
+    document.getElementById('main').addEventListener('click', onClick);
+
+    function onClick(ev) {
+        if (ev.target === encodeSend) {
+            const encodedText = encode(inputArea.value);
+
+            inputArea.value = '';
+            outputArea.value = encodedText;
+
+        } else if (ev.target === decodeRead) {
+
+            const decodedText = decode(outputArea.value);
+
+            outputArea.value = decodedText;
+        }
+    }
+
+    function encode(value) {
+        return encodedText = Array
+            .from(value)
+            .map(x => String.fromCharCode(x.charCodeAt(0) + 1))
+            .join('');
+    }
+
+    function decode(value) {
+        return encodedText = Array
+            .from(value)
+            .map(x => String.fromCharCode(x.charCodeAt(0) - 1))
+            .join('');
+    }
 }
