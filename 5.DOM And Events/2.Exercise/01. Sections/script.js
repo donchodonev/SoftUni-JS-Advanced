580294ca532136b67d5e0447dd8d35ade4d6b903
donchodonev <dummyacc9292@gmail.com>
2021-12-17 08:33:55 +0200

    added first exercise solution

--- 5.DOM And Events/2.Exercise/01. Sections/script.js
+++ 5.DOM And Events/2.Exercise/01. Sections/script.js
@@ -1,3 +1,26 @@
 function create(words) {
-   console.log('TODO:...');
+   const contentTarget = document.getElementById('content');
+   const div = document.createElement('div');
+   const p = document.createElement('p');
+
+   p.style.display = 'none';
+
+   for (const word of words) {
+      let newDiv = div.cloneNode();
+      let newP = p.cloneNode();
+
+      newP.textContent = word;
+      newDiv.appendChild(newP);
+      newDiv.addEventListener('click', onClick);
+
+      contentTarget.appendChild(newDiv);
+   }
+
+   function onClick(ev) {
+      const children = ev.target.childNodes;
+
+      for (const childDiv of children) {
+         childDiv.style.display = 'inline';
+      }
+   }
 }
