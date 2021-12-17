f47a1f28693efb5d5ca2c4648cec6f89ad7efbdf
donchodonev <dummyacc9292@gmail.com>
2021-12-17 10:51:45 +0200

    added another exercise solution

--- 5.DOM And Events/2.Exercise/03. Locked-Profile/app.js
+++ 5.DOM And Events/2.Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,23 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const main = document.getElementById('main');
+
+    main.addEventListener('click', onClick);
+
+    function onClick(ev) {
+        const parent = ev.target.parentNode;
+        const checkedRadio = parent.querySelector('input[value="unlock"]');
+        const showButton = parent.getElementsByTagName('button')[0];
+        const hiddenText = parent.getElementsByTagName('div')[0];
+
+        if (ev.target.nodeName === 'BUTTON' && checkedRadio.checked) {
+
+            if (showButton.textContent == 'Show more') {
+                hiddenText.style.display = 'inline';
+                showButton.textContent = 'Hide it';
+            } else {
+                hiddenText.style.display = 'none';
+                showButton.textContent = 'Show more';
+            }
+        }
+    }
 }
