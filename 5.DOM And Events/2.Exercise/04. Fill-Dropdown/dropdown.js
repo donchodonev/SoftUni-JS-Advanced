8e8be796792359d99569a97d9633a3be132412d3
donchodonev <dummyacc9292@gmail.com>
2021-12-17 11:14:36 +0200

    added another exercise solution

--- 5.DOM And Events/2.Exercise/04. Fill-Dropdown/dropdown.js
+++ 5.DOM And Events/2.Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,17 @@
 function addItem() {
-    console.log('TODO:...');
+    const addButton = document.querySelector('input[value="Add"]');
+
+    const text = document.getElementById('newItemText');
+    const value = document.getElementById('newItemValue');
+
+    const option = document.createElement('option');
+    option.textContent = text.value;
+    option.value = value.value;
+
+    const selectMenu = document.getElementById('menu');
+
+    selectMenu.appendChild(option);
+
+    text.value = '';
+    value.value = '';
 }
